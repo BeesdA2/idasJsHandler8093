4e2a318765ebc7c6d495bacceca69dc35a3b6941
mvmeel05 <m.van.meel@beesda2.nl>
2024-08-30 14:16:22 +0200

    proefrit toegevoegd

--- modules/handtekeningInPDF.js
+++ modules/handtekeningInPDF.js
@@ -6,19 +6,18 @@ const { handleHandtekeningPlaatsen } = require("../../../HandtekeningInPDF/js/co
 async function consumeHandtekeningInPDF  (req, res)  {
   
  
- 
   let origineelDocument = req.query.origineelDocument;
   let handTekening      = req.query.handTekeningPNG;
   let xxText            = req.query.xxText;
   let yyText            = req.query.yyText;
-  
+  let proefrit          = req.query.proefrit;
+   
  var d = new Date();
  var datetime = d.toLocaleString(); 	
- console.log(datetime + ' consumeHandtekeningInPDF origineelDocument: ' +origineelDocument + ' handTekening: ' + handTekening + ' xxText: ' +xxText + ' yyText: ' + yyText );
+ console.log(datetime + ' consumeHandtekeningInPDF origineelDocument: ' +origineelDocument + ' handTekening: ' + handTekening + ' xxText: ' +xxText + ' yyText: ' + yyText + ' proefrit: ' + proefrit );
  
-   
  // res.send('Mollie wordt uitgevoerd ; setletter: ' +setletter + ' filiaal: '+ filiaal+ ' ordernr: '+ ordernr);
-  var resolve =  await handleHandtekeningPlaatsen (origineelDocument,handTekening, xxText, yyText);
+  var resolve =  await handleHandtekeningPlaatsen (origineelDocument,handTekening, xxText, yyText, proefrit);
   //res.send('handleO365  is uitgevoerd ; mailnr: ' +mailnr );
   
   return resolve;	
